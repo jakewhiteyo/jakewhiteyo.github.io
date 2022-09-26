1d465670527478588e022c6255d913f801aae1ae
jakewhiteyo <jakewhiteyo@gmail.com>
2022-09-25 20:00:49 -0600

    added buttons

---
+++ src/components/head/Buttons.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './head.css'
+
+const Buttons = () => {
+  return (
+    <div>
+        <button class="resumeButton">Download Resume</button>
+        <button class="contactButton">Contact</button>
+    </div>
+  )
+}
+
+export default Buttons
--- src/components/head/Head.jsx
+++ src/components/head/Head.jsx
@@ -2,18 +2,23 @@ import React, { Component } from 'react'
 import './head.css'
 import jake from '../../assets/jake.png';
 import circle from '../../assets/circle.png';
-
+import Buttons from './Buttons';
 
 export class Head extends Component {
   static propTypes = {}
 
   render() {
     return (
-        <>
-        <img src={jake} class="imgJake center" alt="jake"/>
-        <img src={circle} class="imgCicle center" alt="circle"/>
-        <h1>Jake White</h1>
-        </>
+        <header>
+            <div class="pad">
+                <img src={jake} class="imgJake center" alt="jake"/>
+                <img src={circle} class="imgCicle center" alt="circle"/>
+                <h1>Jake White</h1>
+            </div>
+            <div class="pad">
+                <Buttons/>
+            </div>
+        </header>
     )
   }
 }
