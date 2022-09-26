06c462e1d7b0674127b2570cabe8de18c996336c
jakewhiteyo <jakewhiteyo@gmail.com>
2022-09-26 15:49:00 -0600

    added resume

--- src/components/head/Buttons.jsx
+++ src/components/head/Buttons.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import resume from '../../assets/jake-white-resume-2022.pdf'
 import './head.css'
+
+const downloadResume = () => {
+    console.log('download');
+    fetch(resume).then(response => {
+        response.blob().then(blob => {
+            // Creating new object of PDF file
+            const fileURL = window.URL.createObjectURL(blob);
+            // Setting various property values
+            let alink = document.createElement('a');
+            alink.href = fileURL;
+            alink.download = 'jake-white-resume-2022.pdf';
+            alink.click();
+        })
+    })
+}
 
 const Buttons = () => {
   return (
     <div>
-        <button class="resumeButton">Download Resume</button>
+        <button class="resumeButton" onClick={downloadResume} value="download">Download Resume</button>
         <button class="contactButton">Contact</button>
     </div>
   )
